983f16c05a5a88c4731f1efc90ff631b932fe0e9
Alex Hp <drunkod@example.com>
2025-02-07 18:24:54 +0500

    feat: add signature parameter to mock environment

--- src/mockEnv.ts
+++ src/mockEnv.ts
@@ -38,6 +38,7 @@ if (import.meta.env.DEV) {
                 ['start_param', 'debug'],
                 ['chat_type', 'sender'],
                 ['chat_instance', '8428209589180549439'],
+                ['signature', '89d6079ad6762351f38c6dbbc41bb53048019256a9443988af7a48bcad16ba31'],
             ]).toString();
 
             lp = {
